a42f83521fbb8160a8c5e75202c89193cbacf5b4
Lukas Trümper <lukas.truemper@outlook.de>
2023-05-26 21:46:48 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,8 @@ mkdir -p build && cd build
 cmake -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-    -DLLVM_ENABLE_PIC=1 \
+    -DLLVM_ENABLE_PIC=1 \ \
+    -DCMAKE_CXX_FLAGS='-std=c++17'
     ..
 make -j${CPU_COUNT}
 
